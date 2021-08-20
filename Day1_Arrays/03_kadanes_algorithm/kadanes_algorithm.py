cdf0f36e7a0c63d2f7658dc36e0d69f75f711a6e
jyshtty <56307643+jyshtty@users.noreply.github.com>
2021-08-20 16:50:22 +0530

    Update kadanes_algorithm.py

--- Day1_Arrays/03_kadanes_algorithm/kadanes_algorithm.py
+++ Day1_Arrays/03_kadanes_algorithm/kadanes_algorithm.py
@@ -2,19 +2,19 @@
 Find maximum sum of a subarray in O(N)
 """
 
-# A = [-2,-3,4,-1,-2,1,5,-3]
-# def foo(A):
-#     sum = 0
-#     maxi = -float('inf')
-#     for i in range(len(A)):
-#         if A[i] + sum < 0:
-#             sum = 0
-#         else:
-#             sum = sum + A[i]
-#         maxi = max(sum, maxi)
-#     return maxi
-#
-# print(foo(A))
+A = [-2,-3,4,-1,-2,1,5,-3]
+def foo(A):
+    sum = 0
+    maxi = -float('inf')
+    for i in range(len(A)):
+        if A[i] + sum < 0:
+            sum = 0
+        else:
+            sum = sum + A[i]
+        maxi = max(sum, maxi)
+    return maxi
+
+print(foo(A))
 
 
 # import  protgres
