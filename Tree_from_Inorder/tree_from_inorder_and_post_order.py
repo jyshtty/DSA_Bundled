ac7d3a45a88cf8064acc3ed0b74c2892cd552aec
jyshtty <jyshtty@gmail.com>
2022-07-29 07:31:24 +0530

    Construct Binary Tree Using Inorder and Postorder

--- Tree_from_Inorder/tree_from_inorder_and_post_order.py
+++ Tree_from_Inorder/tree_from_inorder_and_post_order.py
@@ -1,30 +1,25 @@
-# LEetcode solution
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution:
-    def buildTree(self, preorder, inorder):
-        def array_to_tree(left, right):
-            nonlocal preorder_index
-            # if there are no elements to construct the tree
-            if left > right: return None
+    def buildTree(self, inorder, postorder):
+        def construct(left, right):
+            if left > right:
+                return None
 
-            # select the preorder_index element as the root and increment it
-            root_value = preorder[preorder_index]
-            root = TreeNode(root_value)
+            root_Value = postorder.pop()
+            root_Node = TreeNode(root_Value)
 
+            index = hm[root_Value]
 
-            preorder_index += 1
+            root_Node.right = construct(index + 1, right)
+            root_Node.left = construct(left, index - 1)
+            return root_Node
 
-            # build left and right subtree
-            # excluding inorder_index_map[root_value] element because it's the root
-            root.left = array_to_tree(left, inorder_index_map[root_value] - 1)
-            root.right = array_to_tree(inorder_index_map[root_value] + 1, right)
-
-            return root
-
-        preorder_index = 0
-
-        # build a hashmap to store value -> its index relations
-        inorder_index_map = {}
-        for index, value in enumerate(inorder):
-            inorder_index_map[value] = index
-
-        return array_to_tree(0, len(preorder) - 1)
+        hm = {val: idx for idx, val in enumerate(inorder)}
+        return construct(0, len(inorder) - 1)
