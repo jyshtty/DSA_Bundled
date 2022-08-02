992393c687353541a50c123b5d41431665ed869e
jyshtty <56307643+jyshtty@users.noreply.github.com>
2022-08-02 23:29:31 +0530

    Create Insert_into_BST.py

---
+++ Insert_into_BST.py
@@ -0,0 +1,21 @@
+class Solution:
+    def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
+        node = root
+        while node:
+            # insert into the right subtree
+            if val > node.val:
+                # insert right now
+                if not node.right:
+                    node.right = TreeNode(val)
+                    return root
+                else:
+                    node = node.right
+            # insert into the left subtree
+            else:
+                # insert right now
+                if not node.left:
+                    node.left = TreeNode(val)
+                    return root
+                else:
+                    node = node.left
+        return TreeNode(val)
