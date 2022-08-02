d70c017f2a8584d13790c864444c2febfbb964b2
jyshtty <jyshtty@gmail.com>
2022-08-02 23:34:37 +0530

    new

---
+++ Running_median.py
@@ -0,0 +1,35 @@
+class Solution:
+    # @param A : Number of nodes
+    # @param B : m*2 matrix where each row represents edge going from B[m][0] node to B[m][1]
+    # @param s : source node
+    # @param d : destination node
+    # @return an integer shortest distance from s to d
+    def solve(self, A, B, s, d):
+        adj_list = [[] for i in range(A+1)]
+        for i in B:
+            adj_list[i[0]].append(i[1])
+            adj_list[i[1]].append(i[0])
+        print(adj_list)
+        from collections import deque
+        q = deque()
+        visited = [0] * (A+1)
+
+        q.append(s)
+        visited[s] = True
+        distance = 0
+        while q:
+            u = q.popleft()
+            for i in adj_list[u]:
+                if not visited[i]:
+                    q.append(i)
+                    visited[i] = True
+            distance += 1
+            # if visited[d] == True:
+            #     return distance-1
+        return distance - 1
+
+if __name__ == "__main__":
+    A= 4
+    B= [[1, 2],  [2, 3], [3, 4]]
+    obj = Solution()
+    print(obj.solve(A, B, 1, 4))
---
+++ median_of_sorted_array.py
@@ -0,0 +1,8 @@
+def median_of_sorted_array(arr1, arr2):
+    A = arr1
+    B = arr2
+
+    if len(arr1) > len(arr2);
+        A,B = B,A
+
+    total =
