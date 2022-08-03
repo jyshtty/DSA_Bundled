aebf9ec4f0f69aff1c53145dd893db94cd48bf2d
jyshtty <jyshtty@gmail.com>
2022-08-03 07:29:13 +0530

    Adding new files

---
+++ BST/Search_in_BST.py
@@ -0,0 +1,10 @@
+class Solution:
+    def searchBST(self, root, val):
+        while root:
+            if root.val == val:
+                return root
+            if val > root.val:
+                root = root.right
+            else:
+                root = root.left
+        return None
