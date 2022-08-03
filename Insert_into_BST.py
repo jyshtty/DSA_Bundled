f991256336649f3c0489e8a3681c1a71674039ea
jyshtty <56307643+jyshtty@users.noreply.github.com>
2022-08-03 07:16:04 +0530

    Update Insert_into_BST.py

--- Insert_into_BST.py
+++ Insert_into_BST.py
@@ -1,3 +1,18 @@
+# Recursive Solution
+class Solution:
+    def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
+        if not root:
+            return TreeNode(val)
+        
+        if val > root.val:
+            # insert into the right subtree
+            root.right = self.insertIntoBST(root.right, val)
+        else:
+            # insert into the left subtree
+            root.left = self.insertIntoBST(root.left, val)
+        return root
+
+# Iterative Solution
 class Solution:
     def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
         node = root
