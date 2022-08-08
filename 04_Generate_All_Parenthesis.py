1555c99266afd5b64e7197754b12e902eac52f77
jyshtty <jyshtty@gmail.com>
2022-08-08 20:31:25 +0530

    Important

---
+++ 04_Generate_All_Parenthesis.py
@@ -0,0 +1,20 @@
+class Solution:
+    def generateParenthesis(self, n):
+        stack = []
+        left = right = n
+        result = []
+        self.generate(stack, left, right, result)
+        return result
+
+    def generate(self, stack, left, right, result):
+        if left == 0 and right == 0:
+            result.append("".join(stack))
+            return
+        if left != 0:
+            stack.append("(")
+            self.generate(stack, left - 1, right, result)
+            stack.pop()
+        if right > left:
+            stack.append(")")
+            self.generate(stack, left, right - 1, result)
+            stack.pop()
