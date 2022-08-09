0001e66736aa4986708945df223b43ac973d76e1
jyshtty <jyshtty@gmail.com>
2022-08-09 09:49:24 +0530

    Subsets

--- 03_Generate_All_Permutation_with_Repitation.py
+++ 03_Generate_All_Permutation_with_Repitation.py
@@ -8,12 +8,18 @@ def solve(arr):
 
     def generate(index):
         if index == len(curr):
-            ans.append(curr)
+            print(curr)
         for k,v in frequency_map.items():
             if v > 0:
                 curr[index] = k
                 frequency_map[k] -= 1
                 generate(index+1)
                 frequency_map[k] += 1
+
+    generate(0)
+    return ans
+
+if __name__ == "__main__":
+    print(solve([1,2,2]))
 
 
