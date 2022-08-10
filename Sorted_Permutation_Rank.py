b180c84a0e3177f6ba6a7b6877d96f90832b70eb
jyshtty <56307643+jyshtty@users.noreply.github.com>
2022-08-10 10:05:21 +0530

    Create Sorted_Permutation_Rank.py

---
+++ Sorted_Permutation_Rank.py
@@ -0,0 +1,22 @@
+from math import factorial
+class Solution:
+    # @param A : string
+    # @return an integer
+    def findRank(self, A):
+        res = 0
+        s = sorted(A)
+        lna = len(A)
+        e = 1
+        vis = set()
+        for i in A:
+            c = 0
+            for j in s:
+                if j in vis:
+                    continue
+                if j >= i:
+                    vis.add(j)
+                    break
+                c += 1
+            res += c * factorial(lna - e)
+            e += 1
+        return (res + 1) % 1000003
