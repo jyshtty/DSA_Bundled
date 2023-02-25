1bfe5e459212d421e72dbeaca32bd30b71b8e348
jyshtty <56307643+jyshtty@users.noreply.github.com>
2023-02-25 16:06:38 +0530

    Update 09_Palindrome_Partition.py

--- 09_Palindrome_Partition.py
+++ 09_Palindrome_Partition.py
@@ -1,3 +1,21 @@
+# Given a string s, partition s such that every 
+# substring
+#  of the partition is a 
+# palindrome
+# . Return all possible palindrome partitioning of s.
+
+ 
+
+# Example 1:
+
+# Input: s = "aab"
+# Output: [["a","a","b"],["aa","b"]]
+# Example 2:
+
+# Input: s = "a"
+# Output: [["a"]]
+
+
 class Solution:
     def isPalin(self, l, r, s):
         while l < r:
