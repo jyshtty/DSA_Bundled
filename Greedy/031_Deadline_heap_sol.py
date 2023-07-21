d8bc6282c2f0830811aa5e67501f4c5da5477acc
jyshtty <jyshtty@gmail.com>
2023-07-21 19:04:35 +0530

    Greedy probs

--- Greedy/031_Deadline_heap_sol.py
+++ Greedy/031_Deadline_heap_sol.py
@@ -1,41 +1,12 @@
-# max
-# 1e6 - merory limit
-# 2 * 1e8 -- TLE
-# 1e9 operation
-#
-# 2e8 max operation we can perform per sec.
+class AI:
+    def check(self):
+        return "AI's check"
+    def display(self):
+        print(self.check(),end=" ")
 
+class ML(AI):
+    def check(self):
+        return "ML's check"
 
-class Solution:
-    # @param A : list of integers
-    # @param B : list of integers
-    # @return an integer
-    def solve(self, A, B):
-        c = [(A[i], B[i]) for i in range(len(A))]
-        c.sort()
-        sum = 0
-
-        from heapq import heapify, heappop, heappush
-        hp = []
-        heapify(hp)
-        days = 0
-
-        for i in c:
-            time = i[0]
-            if time > days:
-                heappush(hp, i[1])
-                days += 1
-            else:
-                if hp[0] < i[1]:
-                    heappop(hp)
-                    heappush(hp, i[1])
-        for i in hp:
-            sum += i
-            sum = sum % (10**9 + 7)
-        return sum
-
-A = [1,1,5,2,6]
-B = [10,15,100, 100,150]
-# arr = [100, 1 ,1, 0, 0, 1, 1 ]
-obj = Solution()
-print(obj.solve(A, B))
+AI().display()
+ML().display()
