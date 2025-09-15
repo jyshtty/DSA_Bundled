4d15a130487879c3547a4694e88ec929565d2ec8
Ajay Kumar Shetty <56307643+jyshtty@users.noreply.github.com>
2025-09-15 01:45:24 +0000

    Good nodes

---
+++ Topicwise/Trees/Neetcode/08_level_order_traversal.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        from collections import deque
+
+        q = deque([root])
+        res = []
+        while q:
+            size = len(q)
+            ans = []
+            for i in range(size):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+                ans.append(node.val)
+            res.append(ans)
+        return res
