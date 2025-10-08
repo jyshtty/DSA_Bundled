b0e2b43f2742be7aeddf6f47f7ed612c7b2ccc1c
Ajay Kumar Shetty <56307643+jyshtty@users.noreply.github.com>
2025-10-08 05:01:50 +0000

    Added files

---
+++ Topicwise/Trees/Neetcode/13_construct_binary_from_inorder_preorder.py
@@ -0,0 +1,33 @@
+# LEetcode solution
+
+main step - inorder_index_map build a hashmap to store value -> its index relations
+
+class Solution:
+    def buildTree(self, preorder, inorder):
+        def array_to_tree(left, right):
+            nonlocal preorder_index
+            # if there are no elements to construct the tree
+            if left > right: return None
+
+            # select the preorder_index element as the root and increment it
+            root_value = preorder[preorder_index]            # so important . only thing to remember in this program.
+            root = TreeNode(root_value)                      # so important
+
+
+            preorder_index += 1
+
+            # build left and right subtree
+            # excluding inorder_index_map[root_value] element because it's the root
+            root.left = array_to_tree(left, inorder_index_map[root_value] - 1)
+            root.right = array_to_tree(inorder_index_map[root_value] + 1, right)
+
+            return root
+
+        preorder_index = 0
+
+        # build a hashmap to store value -> its index relations
+        inorder_index_map = {}
+        for index, value in enumerate(inorder):
+            inorder_index_map[value] = index
+
+        return array_to_tree(0, len(preorder) - 1)
