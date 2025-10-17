fc2993ffff898e47670c4d6242186c4ad44cc517
Ajay Kumar Shetty <56307643+jyshtty@users.noreply.github.com>
2025-10-17 18:43:18 +0530

    Update kadanes_algorithm.py

--- Topicwise/scaler_and_striver/Striver_DSA_Sheet/Day1_Arrays/03_kadanes_algorithm/kadanes_algorithm.py
+++ Topicwise/scaler_and_striver/Striver_DSA_Sheet/Day1_Arrays/03_kadanes_algorithm/kadanes_algorithm.py
@@ -4,14 +4,11 @@ Find maximum sum of a subarray in O(N)
 
 A = [-2,-3,4,-1,-2,1,5,-3]
 def foo(A):
-    sum = 0
+    sum_ = 0
     maxi = -float('inf')
-    for i in range(len(A)):
-        if A[i] + sum < 0:
-            sum = 0
-        else:
-            sum = sum + A[i]
-        maxi = max(sum, maxi)
+    for x in A:
+        sum_ = max(x, sum_ + x)   # Either start new subarray at x or extend
+        maxi = max(maxi, sum_)
     return maxi
 
 print(foo(A))
