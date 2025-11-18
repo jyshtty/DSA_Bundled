e85edd850b5e3efee4b2957d5ff2e96198620941
Ajay Kumar Shetty <56307643+jyshtty@users.noreply.github.com>
2025-11-18 10:46:43 +0000

    comments

--- Topicwise/DP-Scaler/Aditya_Verma/Knapsack/0-n_Knapsack/0-n_knapsack.py
+++ Topicwise/DP-Scaler/Aditya_Verma/Knapsack/0-n_Knapsack/0-n_knapsack.py
@@ -1,3 +1,23 @@
+# 0-1 Knapsack Problem
+# Given weights and values of n items, put these items in a knapsack of capacity W to get the maximum total value in the knapsack.
+# You cannot break an item, either pick the complete item or don’t pick it (0-1 property).
+# You are given two integer arrays A and B of size N each which represent values and weights associated with N items respectively.
+# Also, you are given an integer C which represents knapsack capacity.
+
+# example: 
+# Input:
+# A = 10
+# B = [ 5 ]
+# C = [ 10 ]
+# Output: 10
+
+# Explanation:
+# We can take the only item available with weight 5 and value 10.
+# So the maximum value we can obtain is 10.
+# Your Task:
+# You don't need to read input or print anything.
+# Your task is to complete the function solve() which takes values and weights of items and knapsack capacity as input parameters and returns the maximum value that can be put in the knapsack.
+
 class Solution:
     # @param A : list of integers
     # @param B : list of integers
