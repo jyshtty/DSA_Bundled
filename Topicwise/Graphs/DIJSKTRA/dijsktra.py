adbe9f052e0a83d56ec398afb12f16788843d934
Ajay Kumar Shetty <56307643+jyshtty@users.noreply.github.com>
2025-12-15 00:39:39 +0000

    added comments

--- Topicwise/Graphs/DIJSKTRA/dijsktra.py
+++ Topicwise/Graphs/DIJSKTRA/dijsktra.py
@@ -1,6 +1,11 @@
 # Undirected weighted graph
 # shortest distance with minimum weight
 
+# Problem Description
+# Given a weighted undirected graph having A nodes (numbered from 0 to A-1) and M edges given in matrix B of size M x 3 where B[i][0] and B[i][1] represents the nodes connected by an edge and B[i][2] represents the weight of the edge.
+# You need to find the shortest distance from the source node C to all other nodes in the graph. If some node is not reachable from the source node, then the distance to that node is -1.      
+# Return an array of size A where the ith element in the array will represent the shortest distance from the source node C to node i.
+
 
 
 class Solution:
@@ -9,42 +14,38 @@ class Solution:
     # @param C : integer
     # @return a list of integers
     def solve(self, A, B, C):
-        graph = [[] for i in range(A)]
+        graph = [[] for _ in range(A)]
 
-        for i in B:
-            graph[i[0]].append([i[1], i[2]])
-            graph[i[1]].append([i[0], i[2]])
+        # Build adjacency list: graph[u].append((v, weight))
+        for u, v, w in B:
+            graph[u].append((v, w))
+            graph[v].append((u, w))
 
-        distance = [float("inf") for i in range(A)]
+        distance = [float("inf") for _ in range(A)]
 
         from heapq import heapify, heappush, heappop
-        q = []
-        heapify(q)
+        heap = []
+        heapify(heap)
 
-        heappush(q, [0, C])
+        heappush(heap, (0, C))
         distance[C] = 0
-        while q:
-            u = heappop(q)
-            d = u[0]
-            n = u[1]
+        while heap:
+            cur_dist, cur_node = heappop(heap)
 
-            if d == distance[n]:          # will make the code faster
-                for i in range(len(graph[n])):
-                    v = graph[n][i]
-                    node = v[0]
-                    weight = v[1]
-                    if distance[n] + v[1] < distance[node]:
-                        distance[node] = distance[n] + v[1]
-                        heappush(q, [distance[node], node])
+            if cur_dist == distance[cur_node]:          # outdated heap entries are ignored
+                for neighbor, weight in graph[cur_node]:
+                    if distance[cur_node] + weight < distance[neighbor]:
+                        distance[neighbor] = distance[cur_node] + weight
+                        heappush(heap, (distance[neighbor], neighbor))
         for i in range(len(distance)):
             if distance[i] == float("inf"):
                 distance[i] = -1
 
         return distance
 
-if __name__ == "__main__":
-    obj = Solution()
-    A = 6
+    if __name__ == "__main__": 
+        obj = Solution()
+        A = 6
     B =[
         [0, 4, 9],
         [3, 4, 6],
