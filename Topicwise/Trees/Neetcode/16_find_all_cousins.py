683a22719aadd58b89a9bea37165b71c57715e4e
Ajay Kumar Shetty <56307643+jyshtty@users.noreply.github.com>
2026-01-02 06:50:49 +0000

    add comments

--- Topicwise/Trees/Neetcode/16_find_all_cousins.py
+++ Topicwise/Trees/Neetcode/16_find_all_cousins.py
@@ -21,18 +21,19 @@ def find_cousins(root: TreeNode, target: int):
         for _ in range(size):
             node, parent = queue.popleft()
 
-            if node.val == target:
-                target_parent = parent
+            if node.val == target: # found target
+                target_parent = parent # record target's parent
 
-            level.append((node, parent))
+            level.append((node, parent)) # store current level nodes with their parents
 
-            if node.left:
-                queue.append((node.left, node))
+            if node.left: # add left child to queue
+                queue.append((node.left, node)) # add right child to queue
             if node.right:
                 queue.append((node.right, node))
 
-        # if target found at this level
-        if target_parent:
+        # if target found at this level, other nodes in this level are cousins
+        # else continue to next level. ingore nodes from previous levels.
+        if target_parent: # return cousins
             return [
                 node.val for node, parent in level
                 if parent != target_parent and node.val != target
