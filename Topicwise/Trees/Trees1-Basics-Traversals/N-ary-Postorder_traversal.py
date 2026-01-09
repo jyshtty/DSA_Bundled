32711bbd42a05ed9440161ffde4e280a55c5b33c
Ajay Kumar Shetty <56307643+jyshtty@users.noreply.github.com>
2026-01-09 00:41:22 +0000

    Add files

--- Topicwise/Trees/Trees1-Basics-Traversals/N-ary-Postorder_traversal.py
+++ Topicwise/Trees/Trees1-Basics-Traversals/N-ary-Postorder_traversal.py
@@ -1,3 +1,14 @@
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children if children is not None else []
+
+# explanation:
+
+
+
+
 class Solution:
     def postorder(self, root):
         if not root:
@@ -9,5 +20,7 @@ class Solution:
             temp = stack.pop()
             if temp:
                 ans.append(temp.val)
-                stack.extend(temp.children)
+                stack.extend(temp.children) # this is beautiful part. we are adding children as it is. so that when we pop we get rightmost child first.
         return ans[::-1]
+        # finally we reverse the ans to get postorder
+   
