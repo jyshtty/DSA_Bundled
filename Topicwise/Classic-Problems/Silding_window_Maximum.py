8f4d2d72d57223c2288158bc8ef3b86eab874829
Ajay Kumar Shetty <56307643+jyshtty@users.noreply.github.com>
2026-01-15 05:06:37 +0000

    sliding window maximum

--- Topicwise/Classic-Problems/Silding_window_Maximum.py
+++ Topicwise/Classic-Problems/Silding_window_Maximum.py
@@ -1,5 +1,32 @@
 # https://www.geeksforgeeks.org/sliding-window-maximum-maximum-of-all-subarrays-of-size-k/
 
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+
+        from collections import deque 
+
+        q = deque()
+        ans = []
+
+        l = len(nums)
+        for i in range(l): # iterate through all elements
+
+            if q and q[0] <= i - k: # step 1 - remove elements not within the window
+                q.popleft()
+            
+            while q and nums[q[-1]] < nums[i]: # step 2 - maintain decreasing order in deque
+                q.pop()
+            
+            q.append(i) # step 3 - add current element index to deque
+
+            if i-k+1 >= 0: # step 4 - add maximum for current window to ans if window is valid.
+                ans.append(nums[q[0]]) 
+
+        return ans
+        
+
+
 from collections import deque
 def sliding_window_maximum(arr, k):
     l = 0
