a6d4dc6a8943f3e130bb2d08d09a5d7c173f542e
Ajay Kumar Shetty <56307643+jyshtty@users.noreply.github.com>
2026-01-25 22:57:19 +0000

    Majority elem

---
+++ Topicwise/Common_Important/03_majority_elem.py
@@ -0,0 +1,23 @@
+class Solution:
+    # @param A : tuple of integers
+    # @return an integer
+    def majorityElement(self, A):
+        A = list(A)
+        candidate = None
+        count = 0
+
+        # Phase 1: Find candidate
+        for num in A:
+            if count == 0:
+                candidate = num
+                count = 1
+            elif num == candidate:
+                count += 1
+            else:
+                count -= 1
+
+        # Phase 2: Verify candidate
+        if A.count(candidate) > len(A) // 2:
+            return candidate
+
+        return -1
